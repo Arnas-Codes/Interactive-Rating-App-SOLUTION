09b9f214d6d839312577dd5592e7a826cbc744be
Arnas Foysal <arnas.codes@gmail.com>
2026-05-04 23:21:01 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Rating from "./components/Rating";
 import Review from "./components/Review";
 
 function App() {
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-grey-950 text-white">
-      <Routes>
-        <Route path="/" element={<Rating />} />
-        <Route path="/review" element={<Review />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="w-full h-screen flex items-center justify-center bg-grey-950 text-white">
+        <Routes>
+          <Route path="/" element={<Rating />} />
+          <Route path="/review" element={<Review />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
